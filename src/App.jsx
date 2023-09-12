d300011cad46cef91597435117a90697a96221d6
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-12 11:43:35 -0300

    SASS

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import {} from 'react'
+
+import './App.scss'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='container'>
+        <section className='estrutura'>
+          <article className='box'>
+            <h1>Nossa estrutura</h1>
+            <img src='https://i0.wp.com/entreseries.com.br/wp-content/uploads/2023/02/Naruto-panini-encontro-sao-paulo.jpg?fit=1200%2C675&ssl=1&resize=350%2C200'/>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+            <a href='#'>
+                veja mais
+              </a>
+          </article>
+        </section>
+      </div>
+    </>
+  )
+}
+
+export default App
